ea77ad5acc125c9088be42f96e5a621688861606
hainh777 <68391535+hainh777@users.noreply.github.com>
2020-09-19 23:37:36 +0700

    add login page and login controller

--- src/main/java/com/javaweb/controller/web/LoginController.java
+++ src/main/java/com/javaweb/controller/web/LoginController.java
@@ -2,7 +2,6 @@ package com.javaweb.controller.web;
 
 import java.io.IOException;
 
-import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,27 +9,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.javaweb.model.UserModel;
-import com.javaweb.service.INewsService;
-@WebServlet(urlPatterns = {"/trang-chu", "/thoat"})
-public class HomeController extends HttpServlet {
-	private static final long serialVersionUID = 7812242845740990735L;
-	
-	@Inject
-	INewsService newsSV;
-	
+@WebServlet(urlPatterns = { "/dang-nhap", "/login" })
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserModel model = new UserModel();
-		model.setFullName("Hai Nonggggg");
-		request.setAttribute("model", model);
-		
-		newsSV.testinject();
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+
+		RequestDispatcher rd = request.getRequestDispatcher("/views/login/login.jsp");
 		rd.forward(request, response);
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 	}
-	
+
 }
