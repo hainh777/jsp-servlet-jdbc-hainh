f9cf715e2ae7bc938f100a405a340def68158748
hainh777 <68391535+hainh777@users.noreply.github.com>
2020-09-19 22:13:34 +0700

    commit servlet weld

--- src/main/java/com/javaweb/controller/admin/HomeController.java
+++ src/main/java/com/javaweb/controller/admin/HomeController.java
@@ -2,6 +2,7 @@ package com.javaweb.controller.admin;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,11 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javaweb.service.INewsService;
+
 @WebServlet(urlPatterns = {"/admin"})
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 7812242845740990735L;
 
+	@Inject
+	INewsService newsSV;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		newsSV.testinject();
 		RequestDispatcher rd = request.getRequestDispatcher("/views/admin/home.jsp");
 		rd.forward(request, response);
 	}
--- src/main/java/com/javaweb/controller/web/HomeController.java
+++ src/main/java/com/javaweb/controller/web/HomeController.java
@@ -2,6 +2,7 @@ package com.javaweb.controller.web;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.javaweb.model.UserModel;
-
+import com.javaweb.service.INewsService;
 @WebServlet(urlPatterns = {"/trang-chu","/dang-nhap","/thoat"})
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 7812242845740990735L;
-
+	
+	@Inject
+	INewsService newsSV;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		UserModel model = new UserModel();
-		model.setFullName("Hai Nong");
+		model.setFullName("Hai Nonggggg");
 		request.setAttribute("model", model);
+		
+		newsSV.testinject();
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
 		rd.forward(request, response);
 	}
